c9b38e9056fc2172070d5afd460feadd516e06f9
marick <mmaarriicckk@gmail.com>
2020-03-18 16:05:33 +0800

    requirements.txt

--- app.py
+++ app.py
@@ -33,15 +33,12 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    input_text = event.message.text
-    if input_text == '@查詢匯率':
-       resp= requests.get('https://tw.rter.info/capi.php')
-       currency_data = resp.json()
-       usd_to_twd = currency_data['USDTWD']['Exrate']
+    message = TextSendMessage(text=event.message.text)
+    if(message=="查詢匯率"){
+        line_bot_api.reply_message(event.reply_token, "laoding...")
+    }
+    
 
-       line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
