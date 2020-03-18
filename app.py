7e5758163c4b5e60a6b391c0a9fb6eb04d9a99ae
marick <marick2003@users.noreply.github.com>
2020-03-18 22:01:26 +0800

    add

--- app.py
+++ app.py
@@ -65,8 +65,8 @@ def dojob():
     scheduler.start()
 
 
-
+dojob()
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    dojob()
+    
