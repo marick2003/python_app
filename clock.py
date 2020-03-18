cfee6a672d94e3b80e15d470063137658ac39584
marick <marick2003@users.noreply.github.com>
2020-03-19 02:19:06 +0800

    add

--- clock.py
+++ clock.py
@@ -2,12 +2,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    url = "https://pythonline2020.herokuapp.com/"
+    conn = urllib.request.urlopen(url)
+        
+    for key, value in conn.getheaders():
+        print(key, value)
 
 sched.start()
