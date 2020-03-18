815ea0575c6122fe8f8b717a1d5b56d8857a2066
marick <mmaarriicckk@gmail.com>
2020-03-18 16:28:22 +0800

    test

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, request, abort
+import os, sys
+
+from flask import Flask, request, abort, jsonify
+import requests
+
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -6,44 +10,54 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('UJFzuSiw3bjf+ODolRmWgzBm9iAEVnQpWeoCMpWgB9ybewND1qzq3mEI5iYZ/08Sv0oGw0xMiem4r3PS7lSIbyMFeoAD9BB1NC2u/UYb7ed/s6aBdS7xVSFn6OkV5xnYO+RW0pUu74YFkNJaUfYtTgdB04t89/1O/w1cDnyilFU=')
-# Channel Secret
-handler = WebhookHandler('9f737ae6235a4b78d2db6ca9f097b5e7')
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None or channel_access_token is None:
+    print('Specify LINE_CHANNEL_SECRET and LINE_CHANNEL_ACCESS_TOKEN as environment variables.')
+    sys.exit(1)
 
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
+
+@app.route('/callback', methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info('Request body: ' + body)
+
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        print('Invalid signature. Please check your channel access token/channel secret.')
         abort(400)
+
     return 'OK'
 
-# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    if message == '@查詢匯率':
-       resp = requests.get('https://tw.rter.info/capi.php')
-       currency_data = resp.json()
-       usd_to_twd = currency_data['USDTWD']['Exrate']
+    input_text = event.message.text
 
-       line_bot_api.reply_message(event.reply_token, usd_to_twd)
-    
-    
+    if input_text == '@查詢匯率':
+        resp = requests.get('https://tw.rter.info/capi.php')
+        currency_data = resp.json()
+        usd_to_twd = currency_data['USDTWD']['Exrate']
 
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
+
+
+if __name__ == '__main__':
+    app.run()
