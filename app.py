dd961c29a0f3c37180bd3a7d0b5ac054867f4cdf
marick <mmaarriicckk@gmail.com>
2020-03-18 17:05:11 +0800

    test

--- app.py
+++ app.py
@@ -10,11 +10,12 @@ from linebot.exceptions import (
 from linebot.models import *
 
 app = Flask(__name__)
-
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 # Channel Access Token
-line_bot_api = LineBotApi('UJFzuSiw3bjf+ODolRmWgzBm9iAEVnQpWeoCMpWgB9ybewND1qzq3mEI5iYZ/08Sv0oGw0xMiem4r3PS7lSIbyMFeoAD9BB1NC2u/UYb7ed/s6aBdS7xVSFn6OkV5xnYO+RW0pUu74YFkNJaUfYtTgdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi(channel_access_token)
 # Channel Secret
-handler = WebhookHandler('9f737ae6235a4b78d2db6ca9f097b5e7')
+handler = WebhookHandler(channel_secret)
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -35,10 +36,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     input_text = event.message.text
-    if input_text == '@查詢匯率':
-        message = TextSendMessage(text=event.message.text)
-        line_bot_api.reply_message(event.reply_token, message)
     
+    if input_text == '@查詢匯率':
+        resp = requests.get('https://tw.rter.info/capi.php')
+        currency_data = resp.json()
+        usd_to_twd = currency_data['USDTWD']['Exrate']
+
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}'))
+
     
 
 
