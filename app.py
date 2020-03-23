b151dc818fce844c7dc9cec1a57df83da686d814
marick <mmaarriicckk@gmail.com>
2020-03-23 16:19:42 +0800

    add

--- app.py
+++ app.py
@@ -45,29 +45,29 @@ def handle_message(event):
     input_text = event.message.text
 
         #stock button
-        message = TemplateSendMessage(
-            alt_text='Buttons template',
-            template=ButtonsTemplate(
-                thumbnail_image_url='https://example.com/image.jpg',
-                title='Menu',
-                text='Please select',
-                actions=[
-                    PostbackTemplateAction(
-                        label='postback',
-                        text='postback text',
-                        data='action=buy&itemid=1'
-                    ),
-                    MessageTemplateAction(
-                        label='message',
-                        text='message text'
-                    ),
-                    URITemplateAction(
-                        label='uri',
-                        uri='http://example.com/'
-                    )
-                    ]
-                )
-        )
+        # message = TemplateSendMessage(
+        #     alt_text='Buttons template',
+        #     template=ButtonsTemplate(
+        #         thumbnail_image_url='https://example.com/image.jpg',
+        #         title='Menu',
+        #         text='Please select',
+        #         actions=[
+        #             PostbackTemplateAction(
+        #                 label='postback',
+        #                 text='postback text',
+        #                 data='action=buy&itemid=1'
+        #             ),
+        #             MessageTemplateAction(
+        #                 label='message',
+        #                 text='message text'
+        #             ),
+        #             URITemplateAction(
+        #                 label='uri',
+        #                 uri='http://example.com/'
+        #             )
+        #             ]
+        #         )
+        # )
         #check button
         D_message=TemplateSendMessage(
              alt_text='查詢匯率',
