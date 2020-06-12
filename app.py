f62d2cfed749159eec1a70ea18c678d05c11b6c6
marick <mmaarriicckk@gmail.com>
2020-06-12 17:17:16 +0800

    addadd

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def handle_message(event):
     #      line_bot_api.reply_message(
     #         event.reply_token,
     #         TextSendMessage(text='小GG'))
-        
+         
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 3000))
     # app.run(host='0.0.0.0', port=port)
